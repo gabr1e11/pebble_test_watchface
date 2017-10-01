d02f640e55c28d7ebfdc19e51656e45efd41b394
Roberto Cano <gabr1e11@hotmail.com>
2017-10-01 10:47:50 +0200

    Removed uneeded file

--- src/c/watchface.c
+++ src/c/watchface.c
@@ -4,31 +4,48 @@
 
 static Window *s_main_window = NULL;
 static TextLayer *s_time_layer = NULL;
+static GFont s_time_font;
+static BitmapLayer *s_background_layer = NULL;
+static GBitmap *s_background_bitmap = NULL;
+static TextLayer *s_weather_layer = NULL;
 
-static void update_time() {
+static void update_time()
+{
     // Get a tm structure
     time_t temp = time(NULL);
     struct tm *tick_time = localtime(&temp);
-  
+
     // Write the current hours and minutes into a buffer
     static char s_buffer[8];
-    strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                            "%H:%M" : "%I:%M", tick_time);
-  
+    strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+
     // Display this time on the TextLayer
     text_layer_set_text(s_time_layer, s_buffer);
-  }
+}
 
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) 
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
     update_time();
 }
 
-static void main_window_load(Window *window) 
+static void main_window_load(Window *window)
 {
     // Get information about the Window
     Layer *window_layer = window_get_root_layer(window);
     GRect bounds = layer_get_bounds(window_layer);
+
+    // Create GBitmap
+    s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+
+    // Create BitmapLayer to display the GBitmap
+    s_background_layer = bitmap_layer_create(bounds);
+
+    // Set the bitmap onto the layer and add to the window
+    bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+    layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
+    // Create GFont
+    s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
 
     // Create the TextLayer with specific bounds
     s_time_layer = text_layer_create(
@@ -37,19 +54,38 @@ static void main_window_load(Window *window)
     // Improve the layout to be more like a watchface
     text_layer_set_background_color(s_time_layer, GColorClear);
     text_layer_set_text_color(s_time_layer, GColorBlack);
-    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+    text_layer_set_font(s_time_layer, s_time_font);
     text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
     update_time();
 
     // Add it as a child layer to the Window's root layer
     layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+
+    // Create temperature Layer
+    s_weather_layer = text_layer_create(
+        GRect(0, PBL_IF_ROUND_ELSE(125, 120), bounds.size.w, 25));
+
+    // Style the text
+    text_layer_set_background_color(s_weather_layer, GColorClear);
+    text_layer_set_text_color(s_weather_layer, GColorWhite);
+    text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+    text_layer_set_text(s_weather_layer, "Loading...");
 }
-    
-static void main_window_unload(Window *window) 
-{  
+
+static void main_window_unload(Window *window)
+{
     // Destroy TextLayer
     text_layer_destroy(s_time_layer);
+
+    // Unload GFont
+    fonts_unload_custom_font(s_time_font);
+
+    // Destroy GBitmap
+    gbitmap_destroy(s_background_bitmap);
+
+    // Destroy BitmapLayer
+    bitmap_layer_destroy(s_background_layer);
 }
 
 static void init()
@@ -58,10 +94,11 @@ static void init()
     s_main_window = window_create();
 
     // Set handlers to manage the elements inside the Window
-    window_set_window_handlers(s_main_window, (WindowHandlers) {
-        .load = main_window_load,
-        .unload = main_window_unload
-    });
+    window_set_window_handlers(s_main_window, (WindowHandlers){
+                                                  .load = main_window_load,
+                                                  .unload = main_window_unload});
+
+    window_set_background_color(s_main_window, GColorBlack);
 
     // Show the Window on the watch, with animated=true
     window_stack_push(s_main_window, true);
